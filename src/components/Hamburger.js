c42f036ae82dbe9b540e33efdae8de7f601a33bd
Faruk <adekoladamilola4@gmail.com>
2023-07-05 18:45:37 +0100

    hamburger issues

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import '../App.css';
+import React, { useState } from "react";
+import "../App.css";
 
 const Hamburger = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,21 +9,36 @@ const Hamburger = () => {
   };
 
   return (
-    <div className="App">
-      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-      <div className={`menu ${isOpen ? 'open' : ''}`}>
-        <span className='close' onClick={handleToggle}>x</span>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+    // <div className="App">
+    //   <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+    //     <div className="line"></div>
+    //     <div className="line"></div>
+    //     <div className="line"></div>
+    //   </div>
+    //   <div className={`menu ${isOpen ? 'open' : ''}`}>
+    //     <span className='close' onClick={handleToggle}>x</span>
+    //     <ul>
+    //       <li>Home</li>
+    //       <li>About</li>
+    //       <li>Contact</li>
+    //     </ul>
+    //   </div>
 
+    // </div>
+    <div className="App">
+      <div
+        className={`nav-icon1 ${isOpen ? "open" : ""}`}
+        onClick={handleToggle}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </div>
   );
 };
