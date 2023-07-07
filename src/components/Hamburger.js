1e9777617f6c074be395cbb747f59ee437489665
Faruk <adekoladamilola4@gmail.com>
2023-07-07 16:37:59 +0100

    correct some exceptions

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "../App.css";
-// import App from "../App";
-// import Home from "./Home";
 import { Link } from "react-router-dom";
 const Hamburger = () => {
   const [isOpen, setIsOpen] = useState(false);
