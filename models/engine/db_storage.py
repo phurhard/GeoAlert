7c110780522cd5f20334aafeaa74a6d23c64dd5b
phurhard <phurhardeen@gmail.com>
2023-10-30 10:13:58 +0100

    Revert "Refactored the codebase for fa nicer ood architecture"

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,22 +7,20 @@ from models.user import User
 from models.locationreminder import LocationReminder
 from models.todo import Todo
 from models.location import Location
-from models.basemodel import Base
-# from os import getenv
+from models.basemodel import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from contextlib import contextmanager
 import logging
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-# from sqlalchemy.orm.exc import NoResultFound
-# from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-classes = {
-    "User": User,
-    "Location": Location,
-    "Todo": Todo,
-    "LocationReminder": LocationReminder
-    }
+classes = {"User": User, "Location": Location, "Todo": Todo,
+        "LocationReminder": LocationReminder}
 
 
 class DBStorage:
@@ -41,23 +39,31 @@ class DBStorage:
             self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
                 USER, PWD, HOST, DB))
         else:
-            # uncomment to make use of mySQL
-            # self.__engine = create_engine(
-            'mysql+mysqldb://admin:GeoAlertAdmin@localhost/GeoAlert'
-            )
+            # uncommemt to male use of mySQL
+            # self.__engine = create_engine('mysql+mysqldb://admin:GeoAlertAdmin@localhost/GeoAlert')
         '''
         self.__engine = create_engine('sqlite:///:GEOALERT.db', echo=True)
-
-    def load(self):
-        """Load the db session"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+
+    @contextmanager
+    def session_scope(self):
+        """Provides a transactional scope around a series of operations"""
+        session = self.__session()
+        try:
+            yield session  # send d session for work
+            session.commit()  # commit everything to database
+        except Exception as e:
+            session.rollback()  # Return to the last transaction<F11>
+            logger.exception("An error occurred during the transaction.")
+            raise e
+        finally:
+            session.close()
+            session.exit()
 
     def all(self, cls=None):
-        """ Query the database for all instances of the cls
+        """ Query the database for all instamces of the cls
         if provided or if not, it'll bring all the data on the database
         """
         new_dict = {}
@@ -88,23 +94,22 @@ class DBStorage:
 
     def clear(self, obj):
         '''Deletes all entries in database
-        should only be used on tasks'''
-        objs = self.__session.query(Todo).all()
+        should only be ysed on tasks'''
+        objs = self.__session.query(Tasks).all()
         for i in objs:
             self.__session.delete(i)
         self.__session.commit()
 
     @staticmethod
     def get(cls, unit=None):
-        """Returns the object based on it's id.
-        will later need to change the id to something
-        that can be easy to implement"""
+        """Returs the object based on it's id.
+        will later need to change the id to something that can be easy to implement"""
         try:
             if cls not in classes.values():
                 return None
             all_cls = models.storage.all(cls)
             if unit is None:
-                return all_cls  # [cls]
+                return all_cls #[cls]
             else:
                 if cls == eval('User'):
                     for value in all_cls.values():
@@ -113,15 +118,17 @@ class DBStorage:
                 for value in all_cls.values():
                     if (value.id == unit):
                         return value
-        except ValueError:
+        except Exception as e:
             raise ValueError
+                      
 
     def count(self, cls=None):
         """Count the number of objects in the storage"""
-        all_classes = classes.values()
+        all_classes  = classes.values()
         count = 0
 
         if not cls:
+ 
             for clss in all_classes:
                 count += len(models.storage.all(clss).values())
         else:
@@ -132,7 +139,8 @@ class DBStorage:
         """Closes the current database session"""
         if self.__session is not None:
             self.__session.close()
-
     def reload(self):
         '''Reloads the database'''
         pass
+
+   
---
+++ models/todo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" The Todo Class"""
+
+
+from datetime import datetime
+import models
+from models.basemodel import BaseModel, Base
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class Todo(BaseModel, Base):
+    __tablename__ = 'todos'
+    id = Column(String(255), primary_key=True)
+    user_name = Column(String(128), ForeignKey('users.username'))
+    # location_id = Column(String(128), ForeignKey('locations.id'))
+    title = Column(String(255), nullable=False)
+    description = Column(String(255))
+    due_date = Column(DateTime)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+
+    user = relationship('User', backref='todos')
+    # location = relationship('Location', backref='todos')
+    def __init__(self, *args, **kwargs):
+        """initializes todo"""
+        super().__init__(*args, **kwargs) 
